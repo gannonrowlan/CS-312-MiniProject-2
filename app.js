18bcde92dd80b02fffe3cf25e25e40a4accb8ab7
“Gannon <gtr37@nau.edu>
2024-09-30 17:12:00 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/random-cocktail", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
+    const cocktail = result.data.drinks[0];
+    res.render("cocktail.ejs", { cocktail });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
